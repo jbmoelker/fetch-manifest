386f1e7da1669396fb75cdbd7dbcc0b49e978157
Jasper Moelker <j.b.moelker@activeids.nl>
2017-10-22 13:50:30 +0200

    Improved error handling

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
+const pkg = require('./package')
 const cheerio = require('cheerio')
 const request = require('request-promise')
 const { parse: parseUrl, URL } = require('url')
+const { send } = require('micro')
+
+function missingParameterError (parameter, type = 'query') {
+    return {
+        code: 'MISSING_PARAMETER',
+        parameter,
+        message: `'${parameter}' ${type} parameter is required.`,
+        docs: `${pkg.homepage}#parameters`
+    }
+}
 
 module.exports = async (req, res) => {
     const siteUrl = parseUrl(req.url, true).query.url
     res.setHeader('Access-Control-Allow-Origin', '*')
+    const errors = []
 
     if (!siteUrl) {
-        return { error: '`url` search query parameter is required.' }
+        errors.push(missingParameterError('url'))
+    }
+    if (errors.length > 0) {
+        return send(res, 400, { errors })
     }
 
     let html
     try {
         html = await request(siteUrl)
     } catch (err) {
-        return { error: `Unable to fetch webpage from '${siteUrl}'` }
+        return send(res, 500, { errors: [
+            { 
+                code: 'FETCH_ERROR', 
+                message: `Unable to fetch webpage from '${siteUrl}'`
+            }
+        ]})
     }
 
     const $ = cheerio.load(html)
     const manifestHref = $('link[rel="manifest"]').attr('href')
 
     if (!manifestHref) {
-        return { error: `'${siteUrl}' has no web app manifest.` }
+        return send(res, 404, { errors: [
+            { 
+                code: 'MISSING_MANIFEST', 
+                message: `'${siteUrl}' has no web app manifest.`
+            }
+        ]})
     }
 
     const manifestUrl = (new URL(manifestHref, siteUrl)).href
@@ -29,17 +54,28 @@ module.exports = async (req, res) => {
     try {
         manifestContent = await request(manifestUrl)
     } catch (err) {
-        return { error: `Unable to fetch web app manifest from '${manifestUrl}'` }
+        return send(res, 500, { errors: [
+            { 
+                code: 'FETCH_ERROR', 
+                message: `Unable to fetch web app manifest from '${manifestUrl}'`
+            }
+        ]})
     }
 
     let manifest
     try {
         manifest = JSON.parse(manifestContent)
     } catch (err) {
-        return { error: `'${manifestUrl}' is not a valid web app manifest.` }
+        return send(res, 500, { errors: [
+            { 
+                code: 'INVALID_MANIFEST', 
+                message: `'${manifestUrl}' is not a valid web app manifest.`
+            }
+        ]})
     }
 
     return {
+        manifestUrl,
         manifest
     }
 }
--- readme.md
+++ readme.md
@@ -2,10 +2,18 @@
 
 **Micro service to fetch the web app manifest of any website or PWA.**
 
-The only required parameter is the website's URL. The server fetches the given webpage,
-finds the manifest linked on the page (`link[rel="manifest"]`) and returns its contents.
+The server fetches the given webpage, finds the manifest linked on the page (`link[rel="manifest"]`) and returns its contents.
 
 Demo: [`fetch-manifest.now.sh/?url=https://www.voorhoede.nl`](https://fetch-manifest.now.sh/?url=https://www.voorhoede.nl).
+
+
+## Parameters
+
+The service can be configured with the following URL query parameters:
+
+Parameter | Description | Example
+--- | --- | ---
+`url` (required) | URL of the web page to find and return the manifest of. | `url=https://www.voorhoede.nl`
 
 
 ## Development

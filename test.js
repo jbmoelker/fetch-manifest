bcc5841616ef9dda94a93f4832a31e023fb6d61b
Jasper Moelker <j.b.moelker@activeids.nl>
2017-11-16 21:04:54 +0100

    Add tests for all error scenarios

--- test.js
+++ test.js
@@ -35,7 +35,27 @@ test('returns manifest of web page', async t => {
     service.close()
 })
 
-test('returns 404 if site has no manifest', async t => {
+test('returns `400` `MISSING_PARAMETER` if request has no `url` parameter', async t => {
+    // use service on mock site:
+    const service = micro(fetchManifest)
+    const url = await listen(service)
+    const response = await request({
+        method: 'GET',
+        uri: `${url}/`,
+        resolveWithFullResponse: true,
+        simple: false,
+    })
+    const { errors } = JSON.parse(response.body)
+
+    // assert service response:
+    t.deepEqual(response.statusCode, 400)
+    t.deepEqual(errors[0].code, 'MISSING_PARAMETER')
+
+    // clean up:
+    service.close()
+})
+
+test('returns `404` `MISSING_MANIFEST` if site has no manifest', async t => {
     // setup mock site to use the service on:
     const mockSite = micro(async (req, res) => micro.send(res, 200, {}))
     const mockSiteUrl = await listen(mockSite)
@@ -60,7 +80,7 @@ test('returns 404 if site has no manifest', async t => {
     service.close()
 })
 
-test('returns 405 if not a GET request', async t => {
+test('returns `405` `METHOD_NOT_ALLOWED` if not a GET request', async t => {
     // setup mock site to use the service on:
     const mockSite = micro(async (req, res) => micro.send(res, 200, {}))
     const mockSiteUrl = await listen(mockSite)
@@ -79,6 +99,97 @@ test('returns 405 if not a GET request', async t => {
     // assert service response:
     t.deepEqual(response.statusCode, 405)
     t.deepEqual(errors[0].code, 'METHOD_NOT_ALLOWED')
+
+    // clean up:
+    mockSite.close()
+    service.close()
+})
+
+test('returns `500` `FETCH_ERROR` if web page is not found', async t => {
+    // setup mock site to use the service on:
+    const mockSite = micro(async (req, res) => micro.send(res, 404, {}))
+    const mockSiteUrl = await listen(mockSite)
+
+    // use service on mock site:
+    const service = micro(fetchManifest)
+    const url = await listen(service)
+    const response = await request({
+        method: 'GET',
+        uri: `${url}/?url=${mockSiteUrl}`,
+        resolveWithFullResponse: true,
+        simple: false,
+    })
+    const { errors } = JSON.parse(response.body)
+
+    // assert service response:
+    t.deepEqual(response.statusCode, 500)
+    t.deepEqual(errors[0].code, 'FETCH_ERROR')
+
+    // clean up:
+    mockSite.close()
+    service.close()
+})
+
+test('returns `500` `FETCH_ERROR` if linked manifest is not found', async t => {
+    // setup mock site to use the service on:
+    const mockSite = micro(async (req, res) => {
+        const manifestUrl = '/manifest-mock.json'
+        if (req.url === '/') {
+            res.setHeader('Content-Type', 'text/html')
+            return micro.send(res, 200, `<link rel="manifest" href="${manifestUrl}">`)
+        }
+        return micro.send(res, 404, {})
+    })
+    const mockSiteUrl = await listen(mockSite)
+
+    // use service on mock site:
+    const service = micro(fetchManifest)
+    const url = await listen(service)
+    const response = await request({
+        method: 'GET',
+        uri: `${url}/?url=${mockSiteUrl}`,
+        resolveWithFullResponse: true,
+        simple: false,
+    })
+    const { errors } = JSON.parse(response.body)
+
+    // assert service response:
+    t.deepEqual(response.statusCode, 500)
+    t.deepEqual(errors[0].code, 'FETCH_ERROR')
+
+    // clean up:
+    mockSite.close()
+    service.close()
+})
+
+test('returns `500` `INVALID_MANIFEST` if linked manifest is invalid', async t => {
+    // setup mock site to use the service on:
+    const mockSite = micro(async (req, res) => {
+        const manifestUrl = '/manifest-mock.json'
+        if (req.url === '/') {
+            res.setHeader('Content-Type', 'text/html')
+            return micro.send(res, 200, `<link rel="manifest" href="${manifestUrl}">`)
+        }
+        if (req.url === manifestUrl) {
+            return micro.send(res, 200, 'mock manifest')
+        }
+    })
+    const mockSiteUrl = await listen(mockSite)
+
+    // use service on mock site:
+    const service = micro(fetchManifest)
+    const url = await listen(service)
+    const response = await request({
+        method: 'GET',
+        uri: `${url}/?url=${mockSiteUrl}`,
+        resolveWithFullResponse: true,
+        simple: false,
+    })
+    const { errors } = JSON.parse(response.body)
+
+    // assert service response:
+    t.deepEqual(response.statusCode, 500)
+    t.deepEqual(errors[0].code, 'INVALID_MANIFEST')
 
     // clean up:
     mockSite.close()
